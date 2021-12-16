3537d52ce92af7aa7adb2d41a36b7e57d6197bfc
phamtanminhtien <phamtanminhtien@gmail.com>
2021-12-16 19:28:34 +0700

    fix worddetectionalgorithm

--- src/utils/wordDetection.js
+++ src/utils/wordDetection.js
@@ -1,23 +1,27 @@
-const wordDetection = (phrase) => {
-  phrase = phrase.toLowerCase();
-  phrase = phrase.replace(/\n|[-!$%^&*()_+|~=`{}\[\]:";<>?,.\/]|[0-9]/g, " ");
+const a = `RegExr was created by gskinner.com, and is proudly hosted by Media Temple.
 
-  let phrase_array = phrase.split(" ");
+Edit the Expression & Text to see matches. Roll over matches or the expression for details. PCRE & JavaScript flavors of RegEx are supported. Validate your expression with Tests mode.
 
-  phrase_array = phrase_array.filter((item) => {
-    return item !== "" && item !== " ";
-  });
-  phrase_array = phrase_array.map((item) => {
-    if (item.charAt(0) == "'" && item.charAt(item.length - 1) == "'")
-      item = item.replace(/'/g, "");
-    return item;
-  });
-  const object_word = {};
-  phrase_array.forEach((i) => {
-    if (object_word[i] == undefined) object_word[i] = 1;
-    else object_word[i]++;
-  });
-  return object_word;
+The side bar includes a Cheatsheet, full Reference, and Help. You can also Save & Share with the Community, and view patterns you create or favorite in My Patterns.
+
+Explore results with the Tools below. Replace & List output custom results. Details lists capture groups. Explain describes your expression in plain English.
+`;
+
+const wordDetection = (text, minLength) => {
+  const regex = new RegExp(
+    `([\\p{Alphabetic}\\p{Mark}\\p{Decimal_Number}\\p{Connector_Punctuation}\\p{Join_Control}]+){${minLength},}`,
+    "gu"
+  );
+  let result;
+  const words = {};
+  while ((result = regex.exec(text))) {
+    const word = result[0].toLowerCase();
+    if (!words[word]) {
+      words[word] = [];
+    }
+    words[word].push(result.index);
+  }
+  return words;
 };
 
 export default wordDetection;
